2e9fa2633c162d8b7e123c5eb62620c9f379fde1
Tom Lasseret <tlassere@k0r4p2.42mulhouse.fr>
2024-02-01 21:26:15 +0100

    fix: debug and norm

--- libft/ft_tab_del.c
+++ libft/ft_tab_del.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 20:43:00 by tlassere          #+#    #+#             */
-/*   Updated: 2024/02/01 21:06:40 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/02/01 21:26:03 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ char	**ft_tab_del(char **tab, int index)
 	int		len;
 
 	new_tab = tab;
-	if (tab)	
+	if (tab)
 	{
 		len = ft_tab_len(tab);
-		if (index < len)
+		if (index >= 0 && index < len)
 		{
 			new_tab = malloc(sizeof(char **) * len);
 			if (new_tab)
 			{
 				ft_memmove(new_tab, tab, sizeof(char **) * index);
-				ft_memmove(new_tab + index, tab + index + 1, sizeof(char ** ) * (len - index));
+				ft_memmove(new_tab + index, tab + index + 1,
+					sizeof(char **) * (len - index));
+				free(*(tab + index));
 				free(tab);
 			}
 		}
