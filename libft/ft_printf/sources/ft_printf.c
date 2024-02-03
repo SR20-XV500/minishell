e646ff185dfdd37df83ce3f7fb6098e19db4d65c
Tom Lasseret <tlassere@k2r4p2.42mulhouse.fr>
2024-02-03 02:03:14 +0100

    bug: fist later print in fd 1

--- libft/ft_printf/sources/ft_printf.c
+++ libft/ft_printf/sources/ft_printf.c
@@ -13,37 +13,50 @@
 #include "../headers/ft_printf.h"
 #include "../headers/ft_printf_utils.h"
 
-static int	ft_print_arg(const char *ptr, size_t *i, int *size, va_list arg)
+static int	ft_print_arg(const char *ptr, int *size, va_list arg, int fd)
 {
 	int	ret;
 
 	ret = -12;
-	if (ft_strcmp_s2(ptr + *i, "%s") == 0)
-		ret = ft_print_str(va_arg(arg, char *), size);
-	if (ft_strcmp_s2(ptr + *i, "%w") == 0)
-		ret = ft_print_tab(va_arg(arg, char **), size);
-	if (!(ft_strcmp_s2(ptr + *i, "%d")) || !(ft_strcmp_s2(ptr + *i, "%i")))
-		ret = ft_print_nbr(va_arg(arg, int), size);
-	if (ft_strcmp_s2(ptr + *i, "%u") == 0)
-		ret = ft_print_uint(va_arg(arg, unsigned int), size);
-	if (ft_strcmp_s2(ptr + *i, "%%") == 0)
-		ret = ft_print_char('%', size);
-	if (ft_strcmp_s2(ptr + *i, "%c") == 0)
-		ret = ft_print_char((char)va_arg(arg, int), size);
-	if (ft_strcmp_s2(ptr + *i, "%p") == 0)
-		ret = ft_print_ptr(va_arg(arg, void *), size);
-	if (ft_strcmp_s2(ptr + *i, "%x") == 0)
-		ret = ft_print_hex(va_arg(arg, int), size, 0);
-	if (ft_strcmp_s2(ptr + *i, "%X") == 0)
-		ret = ft_print_hex(va_arg(arg, int), size, 1);
-	if (ret != -12)
-		return ((*i += 1), ret);
-	return (ft_print_char(ptr[*i], size));
+	if (ft_strcmp_s2(ptr, "%s") == 0)
+		ret = ft_print_str(va_arg(arg, char *), size, fd);
+	if (ft_strcmp_s2(ptr, "%w") == 0)
+		ret = ft_print_tab(va_arg(arg, char **), size, fd);
+	if (!(ft_strcmp_s2(ptr, "%d")) || !(ft_strcmp_s2(ptr, "%i")))
+		ret = ft_print_nbr(va_arg(arg, int), size, fd);
+	if (ft_strcmp_s2(ptr, "%u") == 0)
+		ret = ft_print_uint(va_arg(arg, unsigned int), size, fd);
+	if (ft_strcmp_s2(ptr, "%%") == 0)
+		ret = ft_print_char('%', size, fd);
+	if (ft_strcmp_s2(ptr, "%c") == 0)
+		ret = ft_print_char((char)va_arg(arg, int), size, fd);
+	if (ft_strcmp_s2(ptr, "%p") == 0)
+		ret = ft_print_ptr(va_arg(arg, void *), size, fd);
+	if (ft_strcmp_s2(ptr, "%x") == 0)
+		ret = ft_print_hex(va_arg(arg, int), size, 0, fd);
+	if (ft_strcmp_s2(ptr, "%X") == 0)
+		ret = ft_print_hex(va_arg(arg, int), size, 1, fd);
+	return (ret);
+}
+
+static int	ft_get_fd(const char *format, va_list arg, size_t *i, int old_fd)
+{
+	int	fd;
+
+	fd = old_fd;
+	if (ft_strcmp_s2(format, "%fdp") == 0)
+	{
+		fd = va_arg(arg, int);
+		*i += 4;
+	}
+	return (fd);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	int		size;
+	int		tmp;
+	int		fd;
 	size_t	i;
 	va_list	args;
 
@@ -51,12 +64,17 @@ int	ft_printf(const char *format, ...)
 		return (-1);
 	i = 0;
 	size = 0;
+	fd = 1;
 	va_start(args, format);
 	while (format[i])
 	{
-		if (ft_print_arg(format, &i, &size, args) == -1)
+		fd = ft_get_fd(format, args, &i, fd);
+		if (format[i] == '\0')
+			break ;
+		tmp = ft_print_arg(format + i, &size, args, fd);
+		if (tmp == -1 ||(tmp == -12 && ft_print_char(format[i], &size, fd) == -1))
 			return (-1);
-		i++;
+		i += 1 + tmp != -12;
 	}
 	va_end(args);
 	return (size);
