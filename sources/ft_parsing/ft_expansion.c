301cc1154a60bf890bcb3f5dba617fc268c4359d
Tom Lasseret <tlassere@k1r1p2.42mulhouse.fr>
2024-02-17 12:42:58 +0100

    feat: ft_expansion_get_str

--- sources/ft_parsing/ft_expansion.c
+++ sources/ft_parsing/ft_expansion.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 14:53:49 by tlassere          #+#    #+#             */
-/*   Updated: 2024/02/17 00:06:19 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/02/17 12:41:56 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,85 +15,84 @@
 int	ft_expansion_is_word(const char *str)
 {
 	int	status;
-	
+
 	status = BAD_PARAMETER;
 	if (str)
 	{
 		status = FAIL;
 		if (*str == '$' && (ft_isalpha(*(str + 1))
-			|| *(str + 1) == '_' || *(str + 1) == '?'))
+				|| *(str + 1) == '_' || *(str + 1) == '?'))
 			status = SUCCESS;
 	}
 	return (status);
 }
-
 
 // cet fonction pue la merde elle ne fait pas se qu'il faut, a refaire
 
-int	ft_expansion_str(const char *str, char **new_str)
-{
-	int		status;
-	char	*curent_card;
+//int	ft_expansion_str(const char *str, char **new_str)
+//{
+//	int		status;
+//	char	*curent_card;
 
-	status = BAD_PARAMETER;
-	curent_card = NULL;
-	if (str && new_str)
-	{
-		status = PARSER_EXPANSION_VAR_NO_CHANGE;
-		if (ft_strchr(str, '$'))
-			curent_card = ft_expansion_get_str(str);
-		if (ft_strchr(str, '$') || curent_card)
-		{
-			if (curent_card && (ft_strlen(str) != ft_strlen(curent_card)
-				|| ft_strcmp_s2(curent_card, str) != CMP_EGAL))
-			{
-				*new_str = curent_card;
-				status = PARSER_EXPANSION_VAR_CHANGE; 
-			}
-			else
-				free(curent_card);
-		}
-		else
-			status = MALLOC_FAIL;
-	}
-	return (status);
-}
+//	status = BAD_PARAMETER;
+//	curent_card = NULL;
+//	if (str && new_str)
+//	{
+//		status = PARSER_EXPANSION_VAR_NO_CHANGE;
+//		if (ft_strchr(str, '$'))
+//			curent_card = ft_expansion_get_str(str);
+//		if (ft_strchr(str, '$') || curent_card)
+//		{
+//			if (curent_card && (ft_strlen(str) != ft_strlen(curent_card)
+//				|| ft_strcmp_s2(curent_card, str) != CMP_EGAL))
+//			{
+//				*new_str = curent_card;
+//				status = PARSER_EXPANSION_VAR_CHANGE; 
+//			}
+//			else
+//				free(curent_card);
+//		}
+//		else
+//			status = MALLOC_FAIL;
+//	}
+//	return (status);
+//}
 
-static int	ft_expansion_replace_word_content(t_word *word)
-{
-	int		status;
-	char	*new_str;
+//static int	ft_expansion_replace_word_content(t_word *word)
+//{
+//	int		status;
+//	char	*new_str;
 
-	status = BAD_PARAMETER;
-	new_str = NULL;
-	if (word && word->word)
-	{
-		if (word->type == D_NOT_SET)
-		{
-			status = ft_expansion_str(word->word, &new_str);
-		}
-		if (status != MALLOC_FAIL)
-			status = SUCCESS;
-	}
-	return (status);
-}
+//	status = BAD_PARAMETER;
+//	new_str = NULL;
+//	if (word && word->word)
+//	{
+//		if (word->type == D_NOT_SET)
+//		{
+//			status = ft_expansion_str(word->word, &new_str);
+//		}
+//		if (status != MALLOC_FAIL)
+//			status = SUCCESS;
+//	}
+//	return (status);
+//}
 
-static int	ft_expansion_replace_word(t_list *lst)
-{
-	int	status;
+//static int	ft_expansion_replace_word(t_list *lst)
+//{
+//	int	status;
 
-	status = BAD_PARAMETER;
-	if (lst)
-	{
-		status = SUCCESS;
-		while (lst && status == SUCCESS)
-		{
-			status = ft_expansion_replace_word_content(lst->content);
-			lst = lst->next;
-		}
-	}
-	return (status);
-}
+//	status = BAD_PARAMETER;
+//	if (lst)
+//	{
+//		status = SUCCESS;
+//		while (lst && status == SUCCESS)
+//		{
+//			status = ft_expansion_replace_word_content(lst->content);
+//			lst = lst->next;
+//		}
+//	}
+//	return (status);
+//}
 
 int	ft_expansion(t_data *data)
 {
@@ -102,7 +101,7 @@ int	ft_expansion(t_data *data)
 	status = BAD_PARAMETER;
 	if (data)
 	{
-		status = ft_expansion_replace_word(data->words);
+		//status = ft_expansion_replace_word(data->words);
 	}
 	return (status);
 }
---
+++ sources/ft_parsing/ft_expansion_get.c
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_expansion_get.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 00:06:42 by tlassere          #+#    #+#             */
+/*   Updated: 2024/02/17 12:41:14 by tlassere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	ft_expansion_add_car(char *str, int car, char **buffer)
+{
+	char	*new_str;
+	size_t	size;
+	int		status;
+
+	new_str = NULL;
+	status = BAD_PARAMETER;
+	if (buffer)
+	{
+		if (str == NULL)
+			str = "";
+		status = MALLOC_FAIL;
+		size = ft_strlen(str) + 2;
+		new_str = malloc(size * sizeof(char));
+		if (new_str && buffer)
+		{
+			status = SUCCESS;
+			ft_memmove(new_str, str, size - 2);
+			new_str[size - 2] = car;
+			new_str[size - 1] = '\0';
+		}
+		*buffer = new_str;
+	}
+	return (status);
+}
+
+static int	ft_expansion_get_content_var(t_data *data,
+	const char *name, char **content)
+{
+	int		status;
+	char	*buffer;
+
+	status = BAD_PARAMETER;
+	buffer = NULL;
+	if (name && content && *name)
+	{
+		status = SUCCESS;
+		if (ft_env_get_pos(*(data->env), name + 1) != -1
+			|| ft_strncmp(name, "$?", 3) == CMP_EGAL)
+		{
+			buffer = ft_env_get_content(*(data->env), name + 1);
+			if (buffer == NULL)
+				status = MALLOC_FAIL;
+		}
+		*content = buffer;
+	}
+	return (status);
+}
+
+static int	ft_expansion_get_content(t_data *data, const char *str,
+	char **content, size_t *i)
+{
+	char	*name;
+	int		status;
+	size_t	sub;
+
+	name = NULL;
+	sub = 1;
+	status = MALLOC_FAIL;
+	if (str[sub] == '$')
+		sub++;
+	else
+	{
+		while (ft_isalnum(str[sub]) || str[sub] == '_')
+			sub++;
+	}
+	name = ft_substr(str, 0, sub);
+	if (name)
+	{
+		status = ft_expansion_get_content_var(data, name, content);
+		free(name);
+	}
+	*i += sub - 1;
+	return (status);
+}
+
+static int	ft_expansion_join_var(t_data *data,
+	const char *str, char **buffer, size_t *i)
+{
+	int		status;
+	char	*new_str;
+	char	*content_name;
+
+	status = BAD_PARAMETER;
+	new_str = NULL;
+	content_name = NULL;
+	if (str && data && i && buffer)
+	{
+		status = ft_expansion_get_content(data, str, &content_name, i);
+		if (status == SUCCESS && content_name)
+		{
+			status = MALLOC_FAIL;
+			new_str = ft_strjoin(*buffer, content_name);
+			if (new_str)
+			{
+				status = SUCCESS;
+				*buffer = new_str;
+			}
+		}
+		free(content_name);
+	}
+	return (status);
+}
+
+char	*ft_expansion_get_str(t_data *data, const char *str)
+{
+	char	*new_str;
+	char	*buffer;
+	int		status;
+	size_t	i;
+
+	new_str = NULL;
+	buffer = NULL;
+	status = SUCCESS;
+	i = 0;
+	if (str && data)
+	{
+		while (str[i] && status == SUCCESS)
+		{
+			if (ft_expansion_is_word(str + i) == SUCCESS)
+				status = ft_expansion_join_var(data, str + i, &buffer, &i);
+			else
+				status = ft_expansion_add_car(buffer, str[i], &buffer);
+			if (new_str != buffer)
+				free(new_str);
+			new_str = buffer;
+			i++;
+		}
+	}
+	return (new_str);
+}
