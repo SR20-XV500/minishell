8d89840866fb4288044d3b4c5367a58ed6e27385
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-02-20 15:52:42 +0100

    fix: get same creation of file with open

--- sources/ft_exec/ft_exec_fd.c
+++ sources/ft_exec/ft_exec_fd.c
@@ -6,14 +6,14 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 02:20:14 by tlassere          #+#    #+#             */
-/*   Updated: 2024/02/20 02:27:21 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/02/20 15:50:17 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// TODO: get the same error messages as in bash
-static int	ft_exec_redirect_fd_err(int fd, const char *err_str)
+static int	ft_exec_redirect_fd_err(t_data *data, int fd,
+	const char *err_str, const char *path)
 {
 	int	status;
 
@@ -23,12 +23,29 @@ static int	ft_exec_redirect_fd_err(int fd, const char *err_str)
 		status = FAIL;
 		ft_fprintf(STDERR, "minishell: ");
 		perror(err_str);
+		data->env->exit_status = 1;
+	}
+	else if (ft_is_directory(path) == SUCCESS)
+	{
+		status = FAIL;
+		ft_fprintf(STDERR, "minishell: %s: Is a directory\n", err_str);
+		data->env->exit_status = 1;
 	}
 	return (status);
 }
 
-// TODO: FIX BUG WITH OPEN FILE
-
+/**
+ * in opens for redirection out, flags 0644 is specified.
+ * its meaning is :
+ * 0 : to indicate that it is a permision flag which changes
+ *	the way the prgram includes the following numbers
+ *
+ * 6: to open the user's read and write functions
+ *
+ * 4: to open in read mode in the group
+ *
+ * 4: to open in read mode in the others
+*/
 int	ft_exec_redirect_fd(t_data *data, int type,
 	const char *path, const char *err_str)
 {
@@ -45,16 +62,16 @@ int	ft_exec_redirect_fd(t_data *data, int type,
 		if (type == D_INPUT)
 			open_fd = open(path, O_RDONLY);
 		else if (type == D_OUTPUT_NEW)
-			open_fd = open(path, O_WRONLY | O_CREAT | O_TRUNC);
+			open_fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		else
-			open_fd = open(path, O_WRONLY | O_CREAT | O_APPEND);
+			open_fd = open(path, O_WRONLY | O_CREAT | O_APPEND, 0644);
 		if (open_fd)
 		{
 			if (*change_fd > 2)
 				close(*change_fd);
 			*change_fd = open_fd;
 		}
-		status = ft_exec_redirect_fd_err(open_fd, err_str);
+		status = ft_exec_redirect_fd_err(data, open_fd, err_str, path);
 	}
 	return (status);
 }
