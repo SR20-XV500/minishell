0a897949b2f709a8c9238d257e5cafec73b4630a
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-02-22 00:16:13 +0100

    feat: exec builtin avancement

--- sources/ft_exec/ft_exec_cmd_true.c
+++ sources/ft_exec/ft_exec_cmd_true.c
@@ -6,15 +6,13 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:38:33 by tlassere          #+#    #+#             */
-/*   Updated: 2024/02/21 21:38:42 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/02/22 00:15:42 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //else if (ft_strncmp("exit", cmd.path, 5) == CMP_EGAL)
-//else if (ft_strncmp("unset", cmd.path, 6) == CMP_EGAL)
-//else if (ft_strncmp("env", cmd.path, 4) == CMP_EGAL)
 
 static int	ft_exec_cmd_builtin(t_data *data, const t_cmd_content cmd)
 {
@@ -29,7 +27,10 @@ static int	ft_exec_cmd_builtin(t_data *data, const t_cmd_content cmd)
 		status = ft_pwd(cmd.argv, cmd.envp);
 	else if (ft_strncmp("export", cmd.path, 7) == CMP_EGAL)
 		status = ft_export(cmd.argv, cmd.envp);
-	(void)data;
+	else if (ft_strncmp("unset", cmd.path, 6) == CMP_EGAL)
+		status = ft_unset(cmd.argv, data->env);
+	else if (ft_strncmp("env", cmd.path, 4) == CMP_EGAL)
+		status = ft_env((const char **)cmd.envp);
 	return (status);
 }
 
@@ -42,7 +43,8 @@ int	ft_exec_cmd_true(t_data *data, const t_cmd_content cmd)
 	{
 		status = SUCCESS;
 		if (ft_is_builtin(cmd.path) == SUCCESS)
-			ft_exec_cmd_builtin(data, cmd);
+			status = ft_exec_cmd_builtin(data, cmd);
+		data->env->exit_status = status;
 	}
 	return (status);
 }
