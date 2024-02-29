f58a2942e0e065c39993d41285601be9565ef0b0
Bertrand CHERONNET <>
2024-02-29 21:20:22 +0100

    Feat: Add move from export to env

--- sources/ft_builtins/ft_export.c
+++ sources/ft_builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: bcheronn <bcheronn@student.42mulhouse>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 21:27:00 by bcheronn          #+#    #+#             */
-/*   Updated: 2024/02/28 01:02:44 by bcheronn         ###   ########.fr       */
+/*   Updated: 2024/02/29 21:17:16 by bcheronn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,32 +34,48 @@ static int	ft_export_is_valid(const char *name)
 	return (ret);
 }
 
-// TODO: allocate *export to NULL at init
-// TODO: export VAR:
-// Check if var contains an =
-// * If not check if it is in **envp
-// ** If not add it to **export
-// ** If yes do nothing
-// * If yes check if it exist in **export
-// ** If yes remove it from **export and add it to **envp
-// ** If not check if it exists in **envp
-// *** If not add it to **envp
+static int	ft_export_to_env(char *arg, t_env *env)
+{
+	int	ret;
+
+	ret = FAIL;
+	ret = ft_env_tab_del(&env->export, ft_env_get_name(arg));
+	ret = ft_env_tab_add(&env->envp, arg);
+	return (ret);
+}
+
+static int	ft_export_update_env(char *arg, t_env *env)
+{
+	int	ret;
+
+	ret = FAIL;
+	if (ft_env_tab_get_pos(env->envp, ft_env_get_name(arg)) == -1)
+		ret = ft_env_tab_add(&env->envp, arg);
+	else
+		;
+	return (ret);
+}
 // *** If yes update it in **envp
+
+// TODO: export VAR:
 static int	ft_export_process(char *arg, t_env *env)
 {
 	int	ret;
 
-	(void)env;
 	ret = FAIL;
 	if (ft_export_is_valid(ft_env_get_name(arg)))
 	{
 		if (ft_strchr(arg, '='))
-			ret = SUCCESS;
+		{
+			if (ft_env_tab_get_pos(env->export, ft_env_get_name(arg)) != -1)
+				ret = ft_export_to_env(arg, env);
+			else
+				ret = ft_export_update_env(arg, env);
+		}
 		else
 		{
 			if (ft_env_tab_get_pos(env->envp, arg) == -1)
 				ret = ft_env_tab_add(&env->export, arg);
-			ret = SUCCESS;
 		}
 	}
 	else
@@ -68,6 +84,7 @@ static int	ft_export_process(char *arg, t_env *env)
 	return (ret);
 }
 
+// TODO: Check the various exit codes for ft_export_process
 int	ft_export(char **argv, t_env *env)
 {
 	int	exit_code;
@@ -81,8 +98,7 @@ int	ft_export(char **argv, t_env *env)
 			exit_code = SUCCESS;
 			while (argv[0])
 			{
-				if (ft_export_process(argv[0], env) == FAIL)
-					exit_code = FAIL;
+				exit_code = ft_export_process(argv[0], env);
 				argv++;
 			}
 		}
