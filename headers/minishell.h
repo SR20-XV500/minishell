98c3dd457bb7118a1f61e24f3cd35616bc22495a
Tom Lasseret <tlassere@k0r4p2.42mulhouse.fr>
2024-03-02 18:20:40 +0100

    Feat: move data, env

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 02:16:52 by tlassere          #+#    #+#             */
-/*   Updated: 2024/03/02 02:49:50 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/03/02 18:16:46 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../libft/libft.h"
 # include "ft_env.h"
+# include "ft_data.h"
 # include "ft_parsing.h"
 # include "ft_builtins.h"
 # include "ft_exec.h"
