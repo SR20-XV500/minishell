c1ea7bb1b28c9a1aa12a57d2d781a39fb5a2e46d
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-03-03 19:34:15 +0100

    Fix: minor bug

--- sources/ft_builtins/ft_unset.c
+++ sources/ft_builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 22:39:18 by bcheronn          #+#    #+#             */
-/*   Updated: 2024/03/03 18:22:16 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/03/03 19:33:46 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@ static int	ft_unset_arg(const char *name, t_env *env)
 	int	status;
 
 	status = SUCCESS;
-	if (ft_env_tab_get_pos(env->export, name) != ENV_NOT_SET)
-		status = ft_env_tab_del(&env->export, name);
-	else if (ft_env_tab_get_pos(env->envp, name) != ENV_NOT_SET)
-		status = ft_env_tab_del(&env->envp, name);
-	if (env->path && ft_strncmp("PATH", name, 5) == CMP_EGAL)
+	if (name)
 	{
-		free(env->path);
-		env->path = NULL;
+		if (ft_env_tab_get_pos(env->export, name) != ENV_NOT_SET)
+			status = ft_env_tab_del(&env->export, name);
+		else if (ft_env_tab_get_pos(env->envp, name) != ENV_NOT_SET)
+			status = ft_env_tab_del(&env->envp, name);
+		if (env->path && ft_strncmp("PATH", name, 5) == CMP_EGAL)
+		{
+			free(env->path);
+			env->path = NULL;
+		}
 	}
 	if (status != SUCCESS)
 		status = FAIL;
