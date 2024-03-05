41ed6dc4d81410ea14699e1abd581715301c60be
Bertrand CHERONNET <>
2024-03-05 23:45:21 +0100

    Fix: pwd accept options and returns 0

--- sources/ft_builtins/ft_pwd.c
+++ sources/ft_builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: bcheronn <bcheronn@student.42mulhouse>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 11:48:43 by tlassere          #+#    #+#             */
-/*   Updated: 2024/02/27 16:17:14 by bcheronn         ###   ########.fr       */
+/*   Updated: 2024/03/05 23:41:36 by bcheronn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ int	ft_pwd(char **argv, char **envp)
 	int	ret;
 
 	ret = 0;
-	if (argv && argv[0] && argv[1])
-	{
-		ft_fprintf(STDERR, "minishell: pwd: does not accept options");
-		ret = 2;
-	}
-	else
-		ft_printf("%s\n", ft_pwd_get());
+	if (argv && argv[0])
+		ret = ft_printf("%s\n", ft_pwd_get());
+	if (ret != -1)
+		ret = 0;
 	(void)envp;
 	return (ret);
 }
