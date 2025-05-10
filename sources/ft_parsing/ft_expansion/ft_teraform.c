a287a817d5405275daa96879b2375a78c474c713
Tom Lasseret <tlassere@k1r1p4.42mulhouse.fr>
2025-05-10 13:02:18 +0200

    Feat: g des leaks

--- sources/ft_parsing/ft_expansion/ft_teraform.c
+++ sources/ft_parsing/ft_expansion/ft_teraform.c
@@ -141,24 +141,31 @@ int	join_expand_left_node(t_list **begin, t_teraform *teraform, int type)
 	return (SUCCESS);
 }
 
-int	join_expand_right_node(t_list **begin, t_teraform *teraform, int type)
+int	join_expand_right_node(t_list **begin, t_teraform *teraform, int type, char **newpos)
 {
 	char	*buffer;
 	t_word	*last;
+	size_t	len;
 
 	if (teraform->right)
 	{
 		if (teraform->rjoin && *begin)
 		{
 			last = ft_lstlast(*begin)->content;
+			len = ft_strlen(last->word);
 			buffer = ft_strjoin(last->word, teraform->right);
 			if (buffer == NULL)
 				return (FAIL);
 			free(last->word);
 			last->word = buffer;
+			*newpos = buffer + len;
 		}
 		else
-			return (join_create_set_node(begin, ft_strdup(teraform->right), type));
+		{
+			if (join_create_set_node(begin, ft_strdup(teraform->right), type))	
+				return (FAIL);
+			*newpos = ((t_word *)ft_lstlast(*begin)->content)->word;
+		}
 	}
 	return (SUCCESS);
 }
@@ -186,7 +193,8 @@ int	join_middle(t_list **begin, t_teraform *teraform, int type)
 
 char	*join_nodes(t_list **lst, t_list *newlst, char *newpos)
 {
-	t_word *buffer;
+	t_word	*buffer;
+	t_list	*last;
 
 	if (newlst == NULL)
 	{
@@ -195,6 +203,16 @@ char	*join_nodes(t_list **lst, t_list *newlst, char *newpos)
 		buffer->word = NULL;
 		return (NULL);
 	}
+	last = ft_lstlast(newlst);
+	ft_word_free((*lst)->content);
+	(*lst)->content = newlst->content;	
+	if (last != newlst)
+	{
+		last->next = (*lst)->next;
+		(*lst)->next = newlst->next;
+		*lst = last;
+	}
+	free(newlst);
 	return (newpos);
 }
 
@@ -202,18 +220,20 @@ char	*join_expand(t_list **lst, t_teraform *teraform, char *str)
 {
 	t_list	*newlst;
 	int		type;
+	char	*newpos;
 
 	newlst = NULL;
 	type = ((t_word *)(*lst)->content)->type;
+	newpos = NULL;
 	if (join_expand_left_node(&newlst, teraform, type) == FAIL)
 		return (str);
 	if (join_middle(&newlst, teraform, type) == FAIL)
 		return (ft_words_clear(&newlst), str);
-	if (join_expand_right_node(&newlst, teraform, type) == FAIL)
+	if (join_expand_right_node(&newlst, teraform, type, &newpos) == FAIL)
 		return (ft_words_clear(&newlst), str);
 	if (newlst == NULL)
 		return (NULL);
-	return (join_nodes(lst, newlst, NULL));
+	return (join_nodes(lst, newlst, newpos));
 }
 
 char	*ft_expend_teraform(t_data *data, t_list **lst, char *str, int inquote)
@@ -228,7 +248,8 @@ char	*ft_expend_teraform(t_data *data, t_list **lst, char *str, int inquote)
 	ret = str;
 	if (ft_set_terraform(&teraform, ((t_word *)(*lst)->content)->word, str) == SUCCESS && ft_set_expand(&teraform, data) == SUCCESS)
 		ret = join_expand(lst, &teraform, str);
+	ft_printf("ret: %p\n", ret);
 	ft_printf("%w\n", teraform.sp);
 	ft_free_teraform(&teraform);
-	return (ret); // return the same *str if problem
+	return (ret);
 }
